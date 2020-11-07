ed3c33a6761d5522807fa0f056d9dc69befe8c74
Patrick Shushereba <pshushereba@gmail.com>
2020-11-06 19:28:39 -0800

    Working on websocket connection.

--- api/server.js
+++ api/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
+
+const videoRouter = require("../videos/video-router.js");
+
+app.use(cors());
+app.use("/api/videos", videoRouter);
 
 module.exports = server;
--- videos/video-router.js
+++ videos/video-router.js
@@ -2,13 +2,22 @@ const express = require("express");
 const router = express.Router();
 const server = require("http").Server(router);
 const io = require("socket.io")(server);
+const { v4: uuidV4 } = require("uuid");
 
 router.get("/", (req, res) => {
-  //
+  const newRoomID = uuidV4();
+  res.status(200).json(newRoomID);
+  //res.redirect(`/${newRoomID}`);
 });
 
 router.get("/:room", (req, res) => {
-  res.send({ roomId: req.params.room });
+  res.send({ roomId: req.params.room, userId: 1 });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    console.log(roomId, userId);
+  });
 });
 
 module.exports = router;
