ad56d4e0d6228c1bec54c79d20d0207a37b8aec5
Patrick Shushereba <pshushereba@gmail.com>
2020-11-09 20:40:53 -0800

    Slightly fixed websocket issue.

--- api/server.js
+++ api/server.js
@@ -1,17 +1,30 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
-const server = require("http").Server(app);
+const server = require("http").createServer(app);
 const io = require("socket.io")(server);
+const cors = require("cors");
 const { v4: uuidV4 } = require("uuid");
 
+const testNamespace = io.of("/testing");
+
 // const videoRouter = require("../videos/video-router.js");
-app.use(cors());
+// {
+//   handlePreflightRequest: (req, res) => {
+//     const headers = {
+//       "Access-Control-Allow-Headers": "Content-Type, Authorization",
+//       "Access-Control-Allow-Origin": "http://localhost:3000", //or the specific origin you want to give access to,
+//     };
+//     res.writeHead(200, headers);
+//     res.end();
+//   },
+// }
+//app.use(cors());
+//io.origins = ["http://localhost:3000"];
 // app.use(function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//   res.header("Access-Control-Allow-Headers", "Content-Type");
-//   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+// res.header("Access-Control-Allow-Headers", "X-Requested-With");
+// res.header("Access-Control-Allow-Headers", "Content-Type");
+// res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
 //   next();
 // });
 
@@ -26,12 +39,8 @@ app.get("/", (req, res) => {
 //   res.send({ roomId: req.params.room, userId: 1 });
 // });
 
-io.on("connection", (socket) => {
-  socket.on("join-room", (messageFromClient) => {
-    console.log(messageFromClient);
-    //socket.join(roomId);
-    //socket.to(roomId).broadcast.emit("user-connected", userId);
-  });
+testNamespace.on("connection", (socket) => {
+  console.log("a user connected");
 });
 
 // app.use("/api/videos", videoRouter);
