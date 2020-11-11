43f63528c02f8044b313c3c809874b0821001c4f
Patrick Shushereba <pshushereba@gmail.com>
2020-11-10 20:10:16 -0800

    Added separate chat and video namespaces.

--- api/server.js
+++ api/server.js
@@ -5,28 +5,10 @@ const io = require("socket.io")(server);
 const cors = require("cors");
 const { v4: uuidV4 } = require("uuid");
 
-const testNamespace = io.of("/testing");
+const chatNsp = io.of("/chat");
+const videoNsp = io.of("/video");
 
-// const videoRouter = require("../videos/video-router.js");
-// {
-//   handlePreflightRequest: (req, res) => {
-//     const headers = {
-//       "Access-Control-Allow-Headers": "Content-Type, Authorization",
-//       "Access-Control-Allow-Origin": "http://localhost:3000", //or the specific origin you want to give access to,
-//     };
-//     res.writeHead(200, headers);
-//     res.end();
-//   },
-// }
-//app.use(cors());
-//io.origins = ["http://localhost:3000"];
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-// res.header("Access-Control-Allow-Headers", "X-Requested-With");
-// res.header("Access-Control-Allow-Headers", "Content-Type");
-// res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-//   next();
-// });
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello");
@@ -35,12 +17,21 @@ app.get("/", (req, res) => {
   // res.redirect(`/${newRoomID}`);
 });
 
+app.get("/new", (req, res) => {
+  const newRoomID = uuidV4();
+  res.status(201).json(newRoomID);
+});
+
 // app.get("/:room", (req, res) => {
 //   res.send({ roomId: req.params.room, userId: 1 });
 // });
 
-testNamespace.on("connection", (socket) => {
-  console.log("a user connected");
+chatNsp.on("connection", (socket) => {
+  console.log("a user connected to the chat");
+});
+
+videoNsp.on("connection", (socket) => {
+  console.log("a user connected to the video");
 });
 
 // app.use("/api/videos", videoRouter);
